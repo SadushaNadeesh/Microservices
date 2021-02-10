98fb88abf1446b38e3ccf7f69b4904db43c3d3ac
SadushaNadeesh <sadusha1994@gmail.com>
2021-02-10 22:05:58 +0530

    microservices course on youtube

---
+++ video_006-git backed cloud config server/config-consumer/src/main/java/com/sadusha/rentcloud/configconsumer/ProfileController.java
@@ -0,0 +1,27 @@
+package com.sadusha.rentcloud.configconsumer;
+
+import com.sadusha.rentcloud.configconsumer.model.MemberProfileConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class ProfileController {
+
+
+    @Autowired
+    MemberProfileConfiguration memberProfileConfiguration;
+
+
+    @RequestMapping("/profile")
+    public String getConfig(Model model){
+
+        model.addAttribute("model",memberProfileConfiguration.getDefaultModel());
+        model.addAttribute("min",memberProfileConfiguration.getMinRentPeriod());
+        return "mprofile";
+
+    }
+
+}
---
+++ video_006-git backed cloud config server/config-consumer/src/test/java/com/sadusha/rentcloud/configconsumer/ConfigConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadusha.rentcloud.configconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
